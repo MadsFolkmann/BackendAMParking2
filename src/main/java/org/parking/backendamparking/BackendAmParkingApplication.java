e3f7d90ddae3dac4819e69451a12d3e10de3c640
Abdiox <124003288+Abdiox@users.noreply.github.com>
2025-04-30 13:54:33 +0200

    Initial commit

---
+++ src/main/java/org/parking/backendamparking/BackendAmParkingApplication.java
@@ -0,0 +1,13 @@
+package org.parking.backendamparking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendAmParkingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendAmParkingApplication.class, args);
+    }
+
+}
