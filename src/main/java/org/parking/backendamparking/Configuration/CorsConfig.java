0c4aa85f9b648a4cb24016aaa6a39dd0f80453f2
MadsFolkmann <124000700+MadsFolkmann@users.noreply.github.com>
2025-06-13 00:40:44 +0200

    Update readme

--- src/main/java/org/parking/backendamparking/Configuration/CorsConfig.java
+++ src/main/java/org/parking/backendamparking/Configuration/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
+                        .allowedOrigins("http://localhost:5173")
                         .allowedMethods("*")
                         .allowedHeaders("*")
                         .allowCredentials(true);
